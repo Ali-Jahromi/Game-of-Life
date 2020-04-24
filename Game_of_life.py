be4a9ad1ab37c53b9714ffef6307f6fe98074665
Ali Jahromi <alisa7420@hotmail.com>
2020-04-24 03:25:42 -0700

    Adding matplot lib for graphical display, and adding comments

--- Game_of_life.py
+++ Game_of_life.py
@@ -1,48 +1,67 @@
 import random
 import time
 import os
+import matplotlib.pyplot as plt
 
+#Fucntion to draw the world of cells
 def draw(u, h, w):
+    #Draw in terminal
+    print("------------------------------") 
     for x in range(30):
         for y in range(30):
             if y != 29:
                 print(u[x][y], end='')
             else:
                 print(u[x][y])
-
+    print("------------------------------")
+    #Draw 2d plot of matrix
+    plt.imshow(u)
+    plt.draw()
+    plt.ion()
+    plt.pause(0.001)
+    plt.clf()
+#Function to evolve the world based on Conway's defined rules
 def evolution(u, h, w):
-    NewWorld = [[" " for x in range(w)] for y in range(h)]
+    #Drawing a temporary world to make to evolve
+    NewWorld = [[0 for x in range(w)] for y in range(h)]
     for x in range(30):
         for y in range(30):
             alivecells = 0
+            #Checking all surrouding cells to cound alive ones
             for xd in range(x-1, x+2):
                 for yd in range(y-1, y+2):
-                    if u[(yd + h) % h][(xd + w) % w] == "+":
+                    if u[(yd + h) % h][(xd + w) % w] == 1:
                         alivecells += 1
-            if u[y][x] == "+":
+            #If the centering cell is alive reduce the number of alive cells by 1 
+            if u[y][x] == 1:
                 alivecells -= 1
-            if alivecells == 3 or (alivecells == 2 and u[y][x] == "+"):
-                NewWorld[y][x] = "+"
+            #Rules 2, and 4 from Wikipedia
+            if alivecells == 3 or (alivecells == 2 and u[y][x] == 1):
+                NewWorld[y][x] = 1
     draw(NewWorld, h, w)
+    #Copying temporary world to the main world
     for x in range(30):
         for y in range(30):
-            world [x][y] = NewWorld[x][y]
+            worldnum [x][y] = NewWorld[x][y]
 
 
 w = 30
 h = 30
-
+#Generating the main world by 30*30 dimension
 world = [[int((random.random()*100)%10) for x in range(w)] for y in range(h)]
+worldnum = [[0 for x in range(w)] for y in range(h)]
+
 for x in range(30):
     for y in range(30):
         if world[x][y] > 1:
-            world[x][y] = ' '
+            worldnum[x][y] = 0
         else:
-            world[x][y] = '+'
-#draw(world, w, h)
+            worldnum[x][y] = 1
+draw(worldnum, h , w)
+
+
 while 1:
-    print("-------------------------------------------")
-    evolution(world, w, h)
+    evolution(worldnum, w, h)
     time.sleep(.2)
     os.system('clear')
 
